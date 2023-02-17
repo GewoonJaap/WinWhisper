3930c66cb661c58654bc91d77f4213b76f183816
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2023-02-17 16:34:05 +0100

    App

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WhisperAI;
+
+Console.WriteLine("Hello, World!");
+
+var audioProcessor = new AudioProcessor();
+await audioProcessor.ProcessAudio();
---
+++ WhisperAI/AudioProcessor.cs
@@ -0,0 +1,34 @@
+﻿using Whisper.net;
+using Whisper.net.Ggml;
+
+namespace WhisperAI
+{
+    public class AudioProcessor
+    {
+        async public Task ProcessAudio()
+        {
+
+            var modelName = "ggml-base.bin";
+            if (!File.Exists(modelName))
+            {
+                using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(GgmlType.Base);
+                using var fileWriter = File.OpenWrite(modelName);
+                await modelStream.CopyToAsync(fileWriter);
+            }
+
+            var builder = WhisperProcessorBuilder.Create()
+                .WithFileModel("C:/Users/Jaap/source/repos/WinWhisper/ConsoleApp/bin/Debug/net7.0/ggml-base.bin")
+                .WithSegmentEventHandler(OnNewSegment)
+                .WithLanguage("auto");
+            using var processor = builder.Build();
+
+            void OnNewSegment(object sender, OnSegmentEventArgs e)
+            {
+                Console.WriteLine($"CSSS {e.Start} ==> {e.End} : {e.Segment}");
+            }
+
+            using var fileStream = File.OpenRead("C:/Users/Jaap/Downloads/350dd587e8736453b7770b975979cc5b40d704470259297f93bee626c12b71fb.wav");
+            processor.Process(fileStream);
+        }
+    }
+}
