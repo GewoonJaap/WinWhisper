1490f9e16d646549997c63075cd3f048ed51d55b
Jaap <jaap@mrproper.dev>
2023-03-05 11:16:01 +0100

    Continue

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -19,24 +19,35 @@ using (SentrySdk.Init(o =>
     FolderManager.CreateFolder(FolderManager.WavFolder);
     FolderManager.CreateFolder(FolderManager.Models);
 
-    Console.Clear();
-    Console.WriteLine("Enter the video path...");
-    var videoPath = Console.ReadLine() ?? string.Empty;
-    //if path starts and ends with ", remove
-    videoPath = VideoPathFormatter.formatVideoPath(videoPath);
-
-    Console.WriteLine("In which language code (en,nl etc) is the audio? Leave empty to auto detect");
-    var languageCode = Console.ReadLine() ?? string.Empty;
-    if (languageCode.Length > 2)
+    while (true)
     {
-        languageCode = string.Empty;
+        Console.Clear();
+        Console.WriteLine("Welcome to WinWhisper. Generate subtitles with ease using WhisperAI.");
+        Console.WriteLine("Enter the video path...");
+        var videoPath = Console.ReadLine() ?? string.Empty;
+        //if path starts and ends with ", remove
+        videoPath = VideoPathFormatter.formatVideoPath(videoPath);
+
+        Console.WriteLine("In which language code (en,nl etc) is the audio? Leave empty to auto detect");
+        var languageCode = Console.ReadLine() ?? string.Empty;
+        if (languageCode.Length > 2)
+        {
+            languageCode = string.Empty;
+        }
+
+        var audioPath = Extractor.ExtractAudioFromVideoFile(videoPath);
+
+        var audioProcessor = new AudioProcessor();
+        await audioProcessor.ProcessAudio(audioPath, languageCode);
+        //remove audioPath file
+        File.Delete(audioPath);
+        Console.WriteLine("Finished audio processing");
+
+        Console.WriteLine("Do you want to process another video? (yes/no)");
+        var processAnotherVideo = Console.ReadLine() ?? string.Empty;
+        if (processAnotherVideo.ToLower() == "no" || processAnotherVideo.ToLower() == "n")
+        {
+            break;
+        }
     }
-
-    var audioPath = Extractor.ExtractAudioFromVideoFile(videoPath);
-
-    var audioProcessor = new AudioProcessor();
-    await audioProcessor.ProcessAudio(audioPath, languageCode);
-    //remove audioPath file
-    File.Delete(audioPath);
-    Console.WriteLine("Finished audio processing");
 }
