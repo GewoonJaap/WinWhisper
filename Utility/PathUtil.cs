752072944dcb0db00e2ee96849b5308a9d8579b6
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2023-11-10 16:15:02 +0100

    Fix path formatting + add testing video

---
+++ Utility/PathUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class PathUtil
+    {
+
+        public static string FormatPath(string path)
+        {
+            //strip first and last " if exists
+            if (path.StartsWith("\""))
+                path = path.Substring(1);
+            if (path.EndsWith("\""))
+                path = path.Substring(0, path.Length - 1);
+            //strip last \ if exists
+            if (path.EndsWith("\\"))
+                path = path.Substring(0, path.Length - 1);
+            return path;
+        }
+    }
+}
